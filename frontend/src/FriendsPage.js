a7357bca13046e5eed96f4563873122f1257fc3d
awaadh0530 <ASO22268@uga.edu>
2026-03-19 12:00:48 -0400

    Update FriendsPage.js

--- frontend/src/FriendsPage.js
+++ frontend/src/FriendsPage.js
@@ -1,2 +1,18 @@
-import 
-import
+import React from 'react'
+import { SampleFriendList } from ''
+
+function FriendsPages() {
+  const [friends, setFriends] = useState([
+    "Seth"
+    "Lior"
+    "Awaadh"
+    "Arshan"
+    "Moosa"
+    "Avash"
+  ])
+  const addFriend = () => {
+    if (newFriend.trim() == "") return;
+    setFriends([...friends, newFriend];
+    setNewFriend("");
+  };
+  

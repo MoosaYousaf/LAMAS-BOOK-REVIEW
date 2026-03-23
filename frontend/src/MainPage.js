d7b432ef18e1b195cfd1f6a839a2a8fd7bc9b6f3
awaadh0530 <ASO22268@uga.edu>
2026-03-23 11:49:24 -0400

    Update MainPage.js

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,1 +1,27 @@
+import React from "react"
 
+const books = [
+  {
+    id: 1,
+    title: "The Hate U Give:,
+    author:
+    cover:
+  },
+
+  {
+    id: 2
+    title: "Mice of Men"
+    author:
+    cover:
+  },
+
+  {
+    id: 3
+    title: "The Great Gatsby"
+    author:
+    cover:
+  },
+
+  
+
+      

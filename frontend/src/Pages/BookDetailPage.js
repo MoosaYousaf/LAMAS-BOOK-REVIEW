4076a6c8dfe9f214804756bd716ccb46ecbcc647
awaadh0530 <ASO22268@uga.edu>
2026-04-13 11:19:56 -0400

    Update BookDetailPage.js

--- frontend/src/Pages/BookDetailPage.js
+++ frontend/src/Pages/BookDetailPage.js
@@ -1,1 +1,10 @@
+import React, {useState, useEffect} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
+const FEATURED_BOOKS = [
+  {
+    id: '1',
+    title: 'The Hate U Give',
+    author: 'Angie Thomas',
+    descripition: 'A young adult about 16-year-old Starr Carter, who navigates two worlds—her poor, Black neighborhood and a wealthy, white prep school—until she witnesses the fatal shooting of her unarmed best friend by a police officer, forcing her to find her voice and confront racism, police brutality, and activism.'
+    price: '$11.99'

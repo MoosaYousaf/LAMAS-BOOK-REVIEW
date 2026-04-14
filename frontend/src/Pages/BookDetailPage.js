f84dfb568ee236c48f18bd9789becbad0ce16c2b
awaadh0530 <ASO22268@uga.edu>
2026-04-14 12:57:03 -0400

    Update BookDetailPage.js

--- frontend/src/Pages/BookDetailPage.js
+++ frontend/src/Pages/BookDetailPage.js
@@ -4,13 +4,19 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 const BOOKS_INTO_DETAIL = [
   {
-    id: '1',
-    title: 'The Hate U Give',
-    author: 'Angie Thomas',
-    stars: '4.45',
+    id: 1,
+    title: "The Hate U Give",
+    author: "Angie Thomas",
+    stars: "4.45",
     summary: "A young adult novel following a 16-year-old Starr Carter, who navigates between her poor neighborhood and an elite prep school. Her life changes when she witnesses the fatal police shooting of her unarmed childhood friend, Khalil. Starr must find her voice to testify against the officer, battling trauma and pressure from her community to speak up against injustice.",
-    reviews: '80,133 reviews'
-  }
+    reviews: "80,133 reviews",
+    pages: "444"
+  },
+
+  {
+    id: 2,
+    title:
+    auth
 
   
   }

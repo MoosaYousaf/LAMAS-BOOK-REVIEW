ae0dcbf5d7bf2c470d4383ebda35273eb5ac645c
awaadh0530 <Awaadhso.530@gmail.com>
2026-04-14 22:06:15 -0400

    Update SettingPage.js

--- frontend/src/Pages/SettingPage.js
+++ frontend/src/Pages/SettingPage.js
@@ -1,1 +1,14 @@
+import React, { useState } from 'react';
 
+constSettingsPage = () => {
+  // Basic information for profile name, phone number, email, etc...
+  const [profile, setProfile] + useState({
+    name: 'John Smith',
+    email: 'johnsmith.12345@gmail.com',
+    phone: '404-XXX-XXX',
+    
+  const [passwords, setPassword] = useState({
+    currentPassword: '',
+    newPassword: '',
+    retypeNewPassword: '',
+  });
